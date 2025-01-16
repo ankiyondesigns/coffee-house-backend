dd5204b06104479cdb6c7cc32f475c298bb4f20f
danieltsega <danieltsega6658@gmail.com>
2025-01-16 16:22:41 +0300

    feat: add product model

--- coffee/models.py
+++ coffee/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class CoffeeProduct(models.Model):
+    image = models.ImageField(upload_to='coffee_products/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    measurement_grams = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name if self.name else 'Unnamed Coffee Product'
+
