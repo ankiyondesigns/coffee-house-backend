b15f5e42464e3d232736eafc9493ad3739bf7e3e
danieltsega <danieltsega6658@gmail.com>
2025-01-17 01:48:31 +0300

    update: update post model of textarea by ckeditor

--- coffee/models.py
+++ coffee/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class CoffeeProduct(models.Model):
     image = models.ImageField(upload_to='coffee_products/')
@@ -10,3 +15,43 @@ class CoffeeProduct(models.Model):
     def __str__(self):
         return self.name if self.name else 'Unnamed Coffee Product'
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                        .filter(status=Post.Status.PUBLISHED)
+
+class Post(models.Model):
+    
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    
+    
+    banner = models.ImageField(upload_to='blog_banner/')
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                related_name='blog_posts')
+    #body = models.TextField()
+    body = RichTextUploadingField(blank=True, null=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                                choices=Status.choices,
+                                default=Status.DRAFT)
+    
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+                    models.Index(fields=['-publish']),
+        ]
+    
+    def __str__(self):
+        return self.title
