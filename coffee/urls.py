b8d1208c78428a48e17c5046110f20108ac819d9
danieltsega <danieltsega6658@gmail.com>
2025-01-17 01:49:24 +0300

    feat: add blog detail url

--- coffee/urls.py
+++ coffee/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # This points to our homepage
+    path('blog/', views.blog, name='blog'),
+    path('<int:id>/', views.post_detail, name='post_detail')
 ]
