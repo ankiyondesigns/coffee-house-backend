b1e849549476f8ed7dac0d9af1291026e58f03bd
danieltsega <danieltsega6658@gmail.com>
2025-01-17 01:49:05 +0300

    feat: add blog list view and blog detail view

--- coffee/views.py
+++ coffee/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
-from .models import CoffeeProduct
+from django.shortcuts import render, get_object_or_404
+from .models import CoffeeProduct, Post
+from django.http import Http404
 
 def home(request):
     # Get all coffee products from the database
     products = CoffeeProduct.objects.all()
 
+    # Get all blog posts from the database
+    posts = Post.published.all()
+
     # Pass the products to the template context
-    return render(request, 'coffee/home.html', {'products': products})
+    return render(request, 'coffee/home.html', {'products': products, 'posts': posts})
+
+def blog(request):
+    # Get all coffee products from the database
+
+    # Pass the products to the template context
+    return render(request, 'coffee/blog.html', {})
+
+def post_detail(request, id):
+    
+    post = get_object_or_404(Post,
+                                id=id,
+                                status=Post.Status.PUBLISHED)
+
+    
+    return render(request,
+                'coffee/blog.html',
+                            {'post': post})
