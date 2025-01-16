b9204a3cf250f535f1c2afcbfe7a9620210811df
danieltsega <danieltsega6658@gmail.com>
2025-01-17 01:49:44 +0300

    feat: add blog post to the admin

--- coffee/admin.py
+++ coffee/admin.py
@@ -1,6 +1,6 @@
 from django.utils.html import format_html
 from django.contrib import admin
-from .models import CoffeeProduct
+from .models import CoffeeProduct, Post
 
 class CoffeeProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'measurement_grams', 'image_preview')
@@ -15,5 +15,16 @@ class CoffeeProductAdmin(admin.ModelAdmin):
         return 'No image'
 
     image_preview.short_description = 'Image'
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
 
 admin.site.register(CoffeeProduct, CoffeeProductAdmin)
