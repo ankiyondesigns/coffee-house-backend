455d85689f81968837f729bf2eead701566c8486
danieltsega <danieltsega6658@gmail.com>
2025-01-17 03:41:16 +0300

    fix: change id based url to slug based

--- coffee/urls.py
+++ coffee/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # This points to our homepage
     path('blog/', views.blog, name='blog'),
-    path('<int:id>/', views.post_detail, name='post_detail')
+    path('<slug:post>/', views.post_detail, name='post_detail')
 ]
