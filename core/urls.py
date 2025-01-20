2ac5dccfd48c9731ee39305e5fcbb435ec7292b5
danieltsega <danieltsega6658@gmail.com>
2025-01-20 14:48:11 +0300

    fix: fix url order

--- core/urls.py
+++ core/urls.py
@@ -8,16 +8,16 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Our App Urls
-    path('', include('coffee.urls')),
-
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-
     # Password Reset URLS
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # Our App Urls
+    path('', include('coffee.urls')),
+
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 
