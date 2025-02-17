076eeb22a5907fc848ea78a5714c41466aaa9a5b
danieltsega <danieltsega6658@gmail.com>
2025-02-17 23:25:01 +0300

    update: update urls

--- coffee/urls.py
+++ coffee/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # This points to our homepage
     path('blog/', views.blog, name='blog'),
-    path('<slug:post>/', views.post_detail, name='post_detail')
+    path('<slug:post>/', views.post_detail, name='post_detail'),
+
+    path("upload-image/", views.upload_image, name="upload_image"),
+    path("upload-video/", views.upload_video, name="upload_video"),
 ]
